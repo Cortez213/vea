ccb683ca9ccb25b6170196979dd857c45adcaad7
Cortez213 <152037137+Cortez213@users.noreply.github.com>
2024-07-07 10:25:58 -0400

    Add files via upload

---
+++ js/carrito.js
@@ -0,0 +1,20 @@
+// OPEN & CLOSE CART
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("#cart-close");
+
+cartIcon.addEventListener("click", () => {
+  cart.classList.add("active");
+});
+
+closeCart.addEventListener("click", () => {
+  cart.classList.remove("active");
+});
+
+// Start when the document is ready
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", start);
+} else {
+  start();
+}
+
